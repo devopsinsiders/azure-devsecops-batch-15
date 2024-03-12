7fa54610f12de20a0d565cf8d2fa5b9acbf451dc
DevOps Insiders <info@devopsinsiders.com>
2024-03-12 22:13:49 +0530

    added code 0903

--- ClassNotes/14.Terraform-Basics-09-03-2024/mera_pyara_rg.tf
+++ ClassNotes/14.Terraform-Basics-09-03-2024/mera_pyara_rg.tf
@@ -1,3 +1,4 @@
+# Declare the required provider and its version
 terraform {
   required_providers {
     azurerm = {
@@ -7,20 +8,24 @@ terraform {
   }
 }
 
+# Configure the Azure provider
 provider "azurerm" {
   features {}
 }
 
+# Define an Azure resource group named "rg-bhondhulal" in the West Europe region
 resource "azurerm_resource_group" "rg-block-name" {
   name     = "rg-bhondhulal"
   location = "West Europe"
 }
 
+# Define another Azure resource group named "rg-dhondhulal" in the West Europe region
 resource "azurerm_resource_group" "rg-block-name2" {
   name     = "rg-dhondhulal"
   location = "West Europe"
 }
 
+# Define a third Azure resource group named "dhondhu-rg" in the West Europe region
 resource "azurerm_resource_group" "rg-block-name3" {
   name     = "dhondhu-rg"
   location = "West Europe"
